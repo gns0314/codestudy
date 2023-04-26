1c196f60adbe713f628f0722d77cb9253796b0b5
gns0314 <34575297+gns0314@users.noreply.github.com>
2023-04-26 17:45:22 +0900

    Update 홀짝에 따라 다른 값 반환하기.py

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,7 +1,8 @@
 def solution(n):
     answer = ''
     if n % 2 == 1:
-        return sum(range(1, n+1, 2))
+        return sum(range(1, n+1, 2)) #sum 함수를 통해 1부터 n까지 2씩 더해가며 계산 ex)n = 7, 1부터 7까지 1+3+5+7 = 16 
+                                     #n+1 이유는 n+1 전까지만 하기때문에 n까지 반복해주려면 n+1을 해줘야함
     else:
-        return sum(i**2 for i in range(2, n+1, 2))
+        return sum(i**2 for i in range(2, n+1, 2)) 
     return answer
