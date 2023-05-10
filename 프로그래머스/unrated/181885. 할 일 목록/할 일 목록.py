574ccf11022f54bdb0144f066dbe4433b307104d
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-05-10 16:46:06 +0900

    Update 할 일 목록.py

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -4,3 +4,8 @@ def solution(todo_list, finished):
         if finished[i] == 0:
             answer.append(todo_list[i])
     return answer
+
+# 리스트컴프리헨션시
+def solution(todo_list, finished):
+    answer = [todo_list[i] for i in range(len(finished)) if finished[i] == 0]
+    return answer
