23df42c83f2c0fe3756ff009cdac657eff21b413
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-05-17 17:16:08 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    answer = 0
+    b = ''
+    dic = ["aya", "woo", "ye", "ma"]
+    for i in babbling :
+        b = i
+        for j in dic :
+            b = b.replace(j, '_',1)
+            if len(b.replace('_','')) == 0 :
+                answer += 1
+                break
+    return answer
