37c4283f538fae4ce3ac230a939f8bb85a314f0c
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-05-25 18:09:06 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,5 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(n):
+        answer.append(bin(arr1[i] | arr2[i])[2:].zfill(n).replace('b','').replace('0',' ').replace('1','#'))
+    return answer
