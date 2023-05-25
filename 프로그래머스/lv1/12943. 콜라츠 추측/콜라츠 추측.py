342518ce99c4ad77ba1778c663f3d382e3af8fd0
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-05-25 17:00:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = [n]
+    count = 0
+    while(n > 1):
+        if n % 2 == 0:
+            n = n//2
+        else:
+            n = 3 * n + 1
+        count += 1
+    if count > 500:
+        count = -1
+    return count
