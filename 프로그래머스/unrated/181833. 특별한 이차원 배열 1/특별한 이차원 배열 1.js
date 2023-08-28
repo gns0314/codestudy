d42689593ae5af6ee9d7e0fe4d2ca227febbc26c
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-08-29 01:40:40 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.09 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const answer = [];
+    
+    for (let i = 0; i < n; i++) {
+        const row = new Array(n).fill(0);
+        row[i] = 1;
+        answer.push(row);
+    }
+    
+    return answer;
+}
