48cc6640c4168886bc5d08cba89aab1973e8aab4
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-08-29 15:46:31 +0900

    [unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,9 +1,7 @@
 function solution(my_string) {
-    const answer = [];
     const a = [];
     for (let i = 0; i < my_string.length; i++) {
         a.push(my_string.slice(-(i + 1)));
     }
-    answer.push(...a.sort());
-    return answer;
+    return a.sort();
 }
