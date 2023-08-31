8e3cb7bc2f38de1e74ee551ea00a308b84a616d7
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-09-01 02:00:50 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,7 +1,3 @@
 function solution(num_list) {
-    var answer = [];
-    while(num_list.length != 0){
-        answer.push(num_list.pop())
-    }
-    return answer;
+    return num_list.reverse()
 }
