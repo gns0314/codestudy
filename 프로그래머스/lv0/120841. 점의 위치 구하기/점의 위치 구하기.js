66e745874dd24f55aec3c4e9f1209d74088a8baa
Seong Nak Hun <34575297+gns0314@users.noreply.github.com>
2023-09-09 00:05:33 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,17 +1,3 @@
 function solution(dot) {
-    var answer = 0;
-    if(dot[0] > 0 ){
-        if(dot[1] > 0){
-            answer = 1
-        }else{
-            answer = 4
-        }
-    }else{
-        if(dot[1] > 0){
-            answer = 2
-        }else{
-            answer = 3
-        }
-    }
-    return answer;
+    return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] > 0 ? 2 : 3;
 }
